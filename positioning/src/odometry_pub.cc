f59a97d6ccbf70ffe909934cf9a33b3b3c408ebd
ZhuChaozheng <370045744@qq.com>
2022-03-18 09:47:35 +0800

    new tf framework

--- positioning/src/odometry_pub.cc
+++ positioning/src/odometry_pub.cc
@@ -105,9 +105,9 @@ int main(int argc, char** argv) {
          << ret_comm_sub_spin << endl;
   }
   // *********** end thread ************
-  tf::TransformListener car_location;
+  tf::TransformListener car_listener;
   //用来保存寻找到的坐标变换数据
-  tf::StampedTransform tag_1;
+  tf::StampedTransform transform;
   // tf::StampedTransform tag_2;
   // tf::StampedTransform tag_3;
   // tf::StampedTransform tag_4;
@@ -131,152 +131,124 @@ int main(int argc, char** argv) {
   transform_tolerance_.fromSec(0.1);
   tf::TransformBroadcaster odom_broadcaster;
   tf::Transform lastTransfrom_map_in_odom;
-  // (1, 0, 0, 0, -1, 0, 0, 0, -1)
   lastTransfrom_map_in_odom =
       tf::Transform(tf::createQuaternionFromRPY(0, 0, 0), tf::Vector3(0, 0, 0));
-  // x 0-2
-  // y-1.9 0
-  tf::TransformBroadcaster map_hik;
+  tf::TransformBroadcaster base_broadcaster;
+  tf::Transform lastTransfrom_tag_in_base;
+  // (1, 0, 0, 0, -1, 0, 0, 0, -1)
+  lastTransfrom_tag_in_base =
+      tf::Transform(tf::Matrix3x3(1, 0, 0, 0, -1, 0, 0, 0, -1), 
+      tf::Vector3(0, 0, 0));
+
+  
+  // publish the relationship between /map and /hik_camera, static
+  tf::TransformBroadcaster map_hik_broadcaster;
   tf::Transform lastTransfrom_map_in_hik;
   lastTransfrom_map_in_hik = tf::Transform(
       tf::Matrix3x3(1, 0, 0, 0, -1, 0, 0, 0, -1), tf::Vector3(0.5, 2, 4));
+  map_hik_broadcaster.sendTransform(tf::StampedTransform(
+      lastTransfrom_map_in_hik, ros::Time::now(), "map", "/hik_camera"));
+
   while (n.ok()) {
-    // tag_0.child_frame_id_ = "tag_0";
-    try {
-      //寻找坐标变换
-      //   car_location.lookupTransform("hik_camera", "tag_0", ros::Time(0),
-      //   tag_0);
-      car_location.lookupTransform("hik_camera", "tag_1", ros::Time(0), tag_1);
-      // car_location.lookupTransform("hik_camera","tag_2",ros::Time(0),tag_2);
-      // car_location.lookupTransform("hik_camera","tag_3",ros::Time(0),tag_3);
-      /*   /car_location.lookupTransform("hik_camera","tag_4",ros::Time(0),tag_4);
-         car_location.lookupTransform("hik_camera","tag_5",ros::Time(0),tag_5);
-         car_location.lookupTransform("hik_camera","tag_6",ros::Time(0),tag_6);
-         car_location.lookupTransform("hik_camera","tag_7",ros::Time(0),tag_7);
-         car_location.lookupTransform("hik_camera","tag_8",ros::Time(0),tag_8);*/
-      //   car_location.lookupTransform("hik_camera","tag_9",ros::Time(0),tag_9);
+    for (auto iter = car_set.begin(); iter != car_set.end();) {
+      int marker = (*iter).get_marker();
+      double quatx, quaty, quatz, quatw;
+      tf::Quaternion quat;
+      double roll, pitch, yaw;  // save roll pitch yaw
+      float angle;
+      Point2f medianPoint;
+      string front_str_tag = "/tag_";;
+      stringstream ss_tag;
+      // construct tf '/robot_1/odom'
+      ss_tag << front_str_tag << marker;
+      string tf_tag = ss_tag.str();
+      
 
-      for (auto iter = car_set.begin(); iter != car_set.end();) {
-        int marker = (*iter).get_marker();
-        double quatx, quaty, quatz, quatw;
-        tf::Quaternion quat;
-        double roll, pitch, yaw;  // save roll pitch yaw
-        float angle;
-        Point2f medianPoint;
-        if (marker == 1) {
-          quatx = tag_1.getRotation().getX();
-          quaty = tag_1.getRotation().getY();
-          quatz = tag_1.getRotation().getZ();
-          quatw = tag_1.getRotation().getW();
-          medianPoint = Point2f(tag_1.getOrigin().x(), tag_1.getOrigin().y());
-        }
-        // if (marker == 2) {
-        //     quatx = tag_2.getRotation().getX();
-        //     quaty = tag_2.getRotation().getY();
-        //     quatz = tag_2.getRotation().getZ();
-        //     quatw = tag_2.getRotation().getW();
+      string front_str = "/robot_";
+      string end_str_odom = "/odom";
+      stringstream ss_odom;
+      // construct tf '/robot_1/odom'
+      ss_odom << front_str << marker << end_str_odom;
+      string tf_odom = ss_odom.str();
+      odom_broadcaster.sendTransform(tf::StampedTransform(
+          lastTransfrom_map_in_odom, ros::Time::now(), "map", tf_odom));
+      // /robot_1/base_link
+      stringstream ss_base;
+      string end_str_base = "/base_link";
+      ss_base << front_str << marker << end_str_base;
+      string tf_base = ss_base.str();
+      // publish the relationship between /tag_1 and /robot_1/base, static
+      base_broadcaster.sendTransform(tf::StampedTransform(
+          lastTransfrom_tag_in_base, ros::Time::now(), tf_tag, tf_base));
 
-        //     medianPoint = Point2f(tag_2.getOrigin().x(),
-        //             tag_2.getOrigin().y());
-        // }
-        // if (marker == 3) {
-        //     quatx = tag_3.getRotation().getX();
-        //     quaty = tag_3.getRotation().getY();
-        //     quatz = tag_3.getRotation().getZ();
-        //     quatw = tag_3.getRotation().getW();
-        //     medianPoint = Point2f(tag_3.getOrigin().x(),
-        //             tag_3.getOrigin().y());
-        // }
-        quat = tf::Quaternion(quatx, quaty, quatz, quatw);
-        yaw = tf::getYaw(quat);
-
-        // float angle = tag_3.getRotation().getAngle();
-        // tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);//convert
-        angle = convertDegree(yaw);
-        // update the states
-        (*iter).set_angle(angle);
-        (*iter).set_median_point(medianPoint);
-        float velocity = (*iter).get_velocity();
-        float angular_velocity = (*iter).get_angular_velocity();
-
-        geometry_msgs::Quaternion odom_quat =
-            tf::createQuaternionMsgFromYaw(yaw);
-
-        string front_str = "/robot_";
-        string end_str_odom = "/odom";
-        stringstream ss;
-        // construct tf '/robot_1/odom'
-        ss << front_str << marker << end_str_odom;
-        string tf_odom = ss.str();
-
-        // /robot_1/base_link
-        stringstream sss;
-        string end_str_base = "/base_link";
-        sss << front_str << marker << end_str_base;
-        string tf_base = sss.str();
-        // publish the relationship between /map and /robot_1/odom, static
-        odom_broadcaster.sendTransform(tf::StampedTransform(
-            lastTransfrom_map_in_odom, ros::Time::now(), "map", tf_odom));
-        // publish the relationship between /map and /hik_camera, static
-        odom_broadcaster.sendTransform(tf::StampedTransform(
-            lastTransfrom_map_in_hik, ros::Time::now(), "map", "/hik_camera"));
-
-        // first, we'll publish the transform over tf
-        geometry_msgs::TransformStamped odom_trans;
-        odom_trans.header.stamp = currentTime;
-        odom_trans.header.frame_id = tf_odom;
-        odom_trans.child_frame_id = tf_base;
-
-        odom_trans.transform.translation.x = medianPoint.x;
-        odom_trans.transform.translation.y = medianPoint.y;
-        odom_trans.transform.translation.z = 0.0;
-        odom_trans.transform.rotation = odom_quat;
-        // send the transform
-        odom_broadcaster.sendTransform(odom_trans);
-
-        // next, we'll publish the odometry message over ROS
-        nav_msgs::Odometry odom;
-        odom.header.stamp = currentTime;
-        odom.header.frame_id = "map";
-        odom.child_frame_id = tf_base;
-
-        // set the position
-        odom.pose.pose.position.x = medianPoint.x;
-        odom.pose.pose.position.y = medianPoint.y;
-        odom.pose.pose.position.z = 0.0;
-        odom.pose.pose.orientation = odom_quat;
-
-        // set the velocity
-        odom.twist.twist.linear.x = velocity;
-        odom.twist.twist.linear.y = 0;
-        odom.twist.twist.angular.z = angular_velocity;
-
-        // publish the message
-        for (auto odom_publisher = odom_publisher_set_.begin();
-             odom_publisher != odom_publisher_set_.end();) {
-          string front_str = "/robot_";
-          string end_str = "/pose";
-          stringstream ssss;
-          // construct topic '/marker1/cmd_vel'
-          ssss << front_str << marker << end_str;
-          string topic = ssss.str();
-          if ((*odom_publisher).getTopic() == topic) {
-            (*odom_publisher).publish(odom);
-            break;
-          }
-          odom_publisher++;
-        }
-        iter++;
+      try{
+        car_listener.lookupTransform("map", tf_base,  
+                                 ros::Time(0), transform);
       }
-      currentTime = ros::Time::now();
-      consumeTime = currentTime.toSec() - lastTime.toSec();
-      // cout << "odometry fps: " << 1/consumeTime << "Hz" << endl;
-      lastTime = currentTime;
-    } catch (tf2::TransformException& ex) {
-      ROS_ERROR("%s", ex.what());
-      ros::Duration(1.0).sleep();
-      continue;
+      catch (tf::TransformException ex){
+        ROS_ERROR("%s",ex.what());
+        ros::Duration(1.0).sleep();
+      }
+      quatx = transform.getRotation().getX();
+      quaty = transform.getRotation().getY();
+      quatz = transform.getRotation().getZ();
+      quatw = transform.getRotation().getW();
+      medianPoint = Point2f(transform.getOrigin().x(), transform.getOrigin().y());
+     
+      quat = tf::Quaternion(quatx, quaty, quatz, quatw);
+      yaw = tf::getYaw(quat);
+
+      // float angle = tag_3.getRotation().getAngle();
+      // tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);//convert
+      angle = convertDegree(yaw);
+      // update the states
+      (*iter).set_angle(angle);
+      (*iter).set_median_point(medianPoint);
+      float velocity = (*iter).get_velocity();
+      float angular_velocity = (*iter).get_angular_velocity();
+
+      geometry_msgs::Quaternion odom_quat =
+          tf::createQuaternionMsgFromYaw(yaw);
+
+      // next, we'll publish the odometry message over ROS
+      nav_msgs::Odometry odom;
+      odom.header.stamp = currentTime;
+      odom.header.frame_id = "map";
+      odom.child_frame_id = tf_base;
+
+      // set the position
+      odom.pose.pose.position.x = medianPoint.x;
+      odom.pose.pose.position.y = medianPoint.y;
+      odom.pose.pose.position.z = 0.0;
+      odom.pose.pose.orientation = odom_quat;
+
+      // set the velocity
+      odom.twist.twist.linear.x = velocity;
+      odom.twist.twist.linear.y = 0;
+      odom.twist.twist.angular.z = angular_velocity;
+
+      // publish the message
+      for (auto odom_publisher = odom_publisher_set_.begin();
+           odom_publisher != odom_publisher_set_.end();) {
+        string front_str = "/robot_";
+        string end_str = "/pose";
+        stringstream ssss;
+        // construct topic '/marker1/cmd_vel'
+        ssss << front_str << marker << end_str;
+        string topic = ssss.str();
+        if ((*odom_publisher).getTopic() == topic) {
+          (*odom_publisher).publish(odom);
+          break;
+        }
+        odom_publisher++;
+      }
+      iter++;
     }
+    currentTime = ros::Time::now();
+    consumeTime = currentTime.toSec() - lastTime.toSec();
+    // cout << "odometry fps: " << 1/consumeTime << "Hz" << endl;
+    lastTime = currentTime;
+  
     rate.sleep();
   }
   return 0;
